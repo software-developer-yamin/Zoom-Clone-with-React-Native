988c7fefc43eb94867cfbb9d7e6374d2362e77d4
DeveloperYamin <developer.yamin@gmail.com>
2022-03-23 17:19:32 +0600

    creating MeetingRoom screen

---
+++ screens/MeetingRoom.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const MeetingRoom = () => {
+  return (
+    <View>
+      <Text>MeetingRoom</Text>
+    </View>
+  );
+};
+
+export default MeetingRoom;
+
+const styles = StyleSheet.create({});
