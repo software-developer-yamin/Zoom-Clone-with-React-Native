4a45d8c72ff1c7fac87e894b57a295530ea63bb7
DeveloperYamin <developer.yamin@gmail.com>
2022-03-23 10:42:53 +0600

    building header and searchBar component !Done

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,14 +1,29 @@
-import { StyleSheet, Text, View } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import { StyleSheet, Text, View } from "react-native";
 
 const SearchBar = () => {
   return (
-    <View>
-      <Ionicons name="ios-search-sharp" size={24} color="black" />
+    <View style={styles.container}>
+      <Ionicons name="ios-search-sharp" size={20} color="#858585" />
+      <Text style={styles.textSearchBar}>Search</Text>
     </View>
   );
 };
 
 export default SearchBar;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#333333",
+    flexDirection: "row",
+    paddingHorizontal: 10,
+    height: 40,
+    alignItems: "center",
+    borderRadius: 10,
+  },
+  textSearchBar: {
+    color: "#858585",
+    paddingLeft: 10,
+    fontSize: 18,
+  },
+});
