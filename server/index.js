a85f4761cda5da37b16477270f4b1e6a729b414f
DeveloperYamin <developer.yamin@gmail.com>
2022-03-24 10:38:54 +0600

    creating StartMeeting component

--- server/index.js
+++ server/index.js
@@ -6,8 +6,16 @@ const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
+
+io.on('connection', (socket) => {
+     console.log("Someone Connected");
+     socket.on("join-room", ({ roomId, userName }) => {
+          console.log("User joined room");
+          console.log(userName, roomId);
+     });
+});
 
 server.listen(port, () => {
      console.log(`Server is up on port ${port}!`)
-})
+});
